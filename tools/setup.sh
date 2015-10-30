2cbdab94251085b7dcdeee930f624e9e077f1eab
Diogo Teixeira <diogo.teixeira@emailbidding.com>
2015-10-30 09:47:01 +0000

    Added commig-msg hook.

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 cp ../tools/pre-commit ../.git/hooks/pre-commit
+cp ../tools/commit-msg ../.git/hooks/commit-msg
 chmod +x ../.git/hooks/pre-commit
+chmod +x ../.git/hooks/commit-msg
 pushd ../platform/vendor/squizlabs/php_codesniffer/CodeSniffer/Standards
 rm -rf Symfony2
 git clone https://github.com/ebidtech/Symfony2-coding-standard.git Symfony2
